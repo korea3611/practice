ead32819dcdb9bea4a6fada3366de018c0d584dc
korea3611 <korea3611@naver.com>
2021-01-14 10:32:32 +0900

    2021.01.14

--- practice_code.py
+++ practice_code.py
@@ -1,12 +1,34 @@
-x,y = map(int,input().split())
-dis = y-x
-count = 0
-move = 1
-moved = 0
-while moved < dis:
-    count += 1
-    moved += move
-    if count % 2 == 0:
-        move += 1
-print(count)
+import itertools
 
+def baseball_fun(x,y):
+    x = list(x); y = list(y)
+    s = 0; b = 0
+    for i in range(3):
+        if x[i] in y:
+            if y.index(x[i]) == i:
+                s += 1
+            else:
+                b += 1
+    return [s,b]
+
+def solution(baseball_list):
+    v = list(map(lambda x:str(x[0]), baseball_list))
+    r = list(map(lambda x: [x[1], x[2]], baseball_list))
+
+    all = list(itertools.permutations(range(1,10), 3))
+    all = list(map(lambda x: list(map(str, x)), all))
+
+    cnt = 0
+    for x in all:
+        if [baseball_fun(x,y) for y in v] == r:
+            cnt += 1
+
+    print(cnt)
+
+n = int(input())
+baseball_list = []
+for i in range(n):
+    a,b,c = map(int,input().split())
+    baseball_list.append([a,b,c])
+
+solution(baseball_list)
